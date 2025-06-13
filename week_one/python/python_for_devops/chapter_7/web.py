db76b048a11db916512a44e09501ab3605168a14
texashodlr <conorxdevlin@gmail.com>
2025-06-12 20:49:01 -0600

    Functional database curl

--- week_one/python/python_for_devops/chapter_7/web.py
+++ week_one/python/python_for_devops/chapter_7/web.py
@@ -1,7 +1,31 @@
+import time
+import random
 from flask import Response, Flask
 import prometheus_client
+from prometheus_client import Counter, Histogram
 
 app = Flask('prometheus-app')
+
+REQUESTS = Counter(
+        'requests', 'Application Request Count',
+        ['endpoint']
+    )
+TIMER = Histogram(
+        'slow', 'Slow Requests',
+        ['endpoint']
+    )
+
+@app.route('/database/')
+def database():
+    with TIMER.labels('/database').time():
+        # Simulated DB response time
+        sleep(random.uniform(1, 3))
+    return '<h1>Completed expensive database operations</h1>'
+
+@app.route('/')
+def index():
+    REQUESTS.labels(endpoint='/').inc()
+    return '<h1>Development Prometheus-backed Flask App</h1>'
 
 @app.route('/metrics/')
 def metrics():
